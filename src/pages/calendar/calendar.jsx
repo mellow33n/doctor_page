8bad612c6bc6984b1614c3a734256a7e41ec9df5
mellow33n <kramzeee@gmail.com>
2024-05-27 21:53:06 +0300

    add form and 1st step to calendar page

--- src/pages/calendar/calendar.jsx
+++ src/pages/calendar/calendar.jsx
@@ -1,96 +1,46 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import Badge from '@mui/material/Badge';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { PickersDay } from '@mui/x-date-pickers/PickersDay';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
-import { ukUA } from '@mui/x-date-pickers/locales';
-import 'dayjs/locale/uk'
+import * as React from "react";
 
+import { useNavigate } from "react-router";
 
+import { useForm, FormProvider } from "react-hook-form";
 
-
-
-function ServerDay(props) {
-  const { disabledDays = [], day, outsideCurrentMonth, ...other } = props;
-  const isSelected =
-    !props.outsideCurrentMonth && disabledDays.indexOf(props.day.date()) >= 0;
-
-  return (
-    <Badge
-      key={props.day.toString()}
-      overlap="circular"
-      badgeContent={isSelected ? undefined : undefined}
-    >
-      <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
-    </Badge>
-  );
-}
-
+import PickDay from "./pickDay";
 
 export default function Calendar() {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [disabledDays, setDisableDays] = React.useState([1, 2, 15]);
-  const [value, setValue] = React.useState(null);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [currentStep, setCurrentStep] = React.useState(0);
+  const steps = [<PickDay props={{control, errors}}/>];
 
-  const maxDaysNumber = 60;
-
-
-  const handleMonthChange = (date) => {
-    /* console.log(date); */
-
+  const onSubmit = (data) => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    } else {
+      console.log("Form data:", data);
+    }
   };
-  const isDisabledDate = (date) => {
-
-    let isDisabled = false;
-    disabledDays.forEach((value) => {
-      return value === date.$D ? isDisabled = true : value
-    })
-    return isDisabled
-
-  };
-  function getMaxDay(daysNumber = maxDaysNumber) {
-    return dayjs().add(daysNumber, 'day')
-
-  }
-  
 
   return (
-    <LocalizationProvider 
-    dateAdapter={AdapterDayjs}
-    adapterLocale="uk"
-    localeText={
-      ukUA.components.MuiLocalizationProvider.defaultProps.localeText
-    }
-
-    >
-      <DateCalendar
-        views ={['day']}
-        value={value}
-        onChange = {(newValue) => setValue(newValue)}
-        loading={isLoading}
-        onMonthChange={handleMonthChange}
-        renderLoading={() => <DayCalendarSkeleton />}
-        shouldDisableDate={isDisabledDate} 
-        disablePast = {true}
-        displayWeekNumber = {true}
-        focusedView = {'day'}
-        maxDate = {getMaxDay()}
-        dayOfWeekFormatter={(data)=> data}
-        slots={{
-          day: ServerDay,
-        }}
-        slotProps={{
-          day: {
-            disabled: true,
-          },
-          textField: {
-            disabled: true,
-          }
-        }}
-      />
-    </LocalizationProvider>
+    <FormProvider >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {steps[currentStep]}
+        <div className="btn-group">
+          {currentStep > 0 && (
+            <button
+              type="button"
+              onClick={() => setCurrentStep(currentStep - 1)}
+            >
+              Previous
+            </button>
+          )}
+          <button type="submit">
+            {currentStep === steps.length - 1 ? "Submit" : "Next"}
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   );
 }
---
+++ src/pages/calendar/pickDay.jsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import dayjs from "dayjs";
+import Badge from "@mui/material/Badge";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
+import { ukUA } from "@mui/x-date-pickers/locales";
+import { Controller } from "react-hook-form";
+import { FormControl } from "@mui/material";
+import "dayjs/locale/uk";
+
+function ServerDay(props) {
+  const { disabledDays = [], day, outsideCurrentMonth, ...other } = props;
+  const isSelected =
+    !props.outsideCurrentMonth && disabledDays.indexOf(props.day.date()) >= 0;
+
+  return (
+    <Badge
+      key={props.day.toString()}
+      overlap="circular"
+      badgeContent={isSelected ? undefined : undefined}
+    >
+      <PickersDay
+        {...other}
+        outsideCurrentMonth={outsideCurrentMonth}
+        day={day}
+      />
+    </Badge>
+  );
+}
+
+export default function PickDay({ props }) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [disabledDays, setDisableDays] = React.useState([1, 2, 15]);
+  console.log(props);
+
+  const maxDaysNumber = 60;
+
+  const handleMonthChange = (date) => {
+    console.log(date);
+  };
+  const isDisabledDate = (date) => {
+    let isDisabled = false;
+    disabledDays.forEach((value) => {
+      return value === date.$D ? (isDisabled = true) : value;
+    });
+    return isDisabled;
+  };
+  function getMaxDay(daysNumber = maxDaysNumber) {
+    return dayjs().add(daysNumber, "day");
+  }
+
+  return (
+    <Controller
+      name="dayCalendar"
+      control={props.control}
+      render={({ field }) => (
+        <FormControl>
+          <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            adapterLocale="uk"
+            localeText={
+              ukUA.components.MuiLocalizationProvider.defaultProps.localeText
+            }
+          >
+            <DateCalendar
+              views={["day"]}
+              value={field.value || null}
+              onChange={(newValue) => field.onChange(newValue)}
+              loading={isLoading}
+              onMonthChange={(date) => handleMonthChange(date)}
+              renderLoading={() => <DayCalendarSkeleton />}
+              shouldDisableDate={isDisabledDate}
+              disablePast={true}
+              focusedView={"day"}
+              maxDate={getMaxDay()}
+              dayOfWeekFormatter={(data) => data}
+              slots={{
+                day: ServerDay,
+              }}
+              slotProps={{
+                day: {
+                  disabled: true,
+                },
+                textField: {
+                  disabled: true,
+                },
+              }}
+            />
+          </LocalizationProvider>
+        </FormControl>
+      )}
+    />
+  );
+}
